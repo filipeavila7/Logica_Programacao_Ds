51c1d1fd6027ba00e0f966f7147f0e6452d359e8
filipeavila7 <yasminraquelfernandesdeoliveir@gmail>
2024-08-27 08:01:15 -0300

    programa

---
+++ aula03.py
@@ -0,0 +1,34 @@
+num1 = int(input("digite um numero: "))
+num2 = int(input('digite outro numero:'))
+soma = num1 + num2
+div = num1 / num2
+multi = num1 * num2
+sub = num1 - num2
+pot = num1 ** num2
+divintera = num1 // num2 #ignora os numeros depois da virgula 
+modulo = num1 % num2 # resto da divisao 
+
+'''print(num1 > num2) # maior que
+print(num1 < num2) #menor que
+print(num1 == num2) #igual
+print(num1 != num2) #diferente
+print( num1 <= 100) #menor ou igual a um numero'''
+
+print(num1 <= 100 and num2 <=100) #and so sera vdd se as duas forem vdd
+print(num1 <= 100 or num2 <=100) #or so e vdd se um dos 2 for vdd
+print(num1 <= 100 and num2 <=100 and (num1 + num2) > 100)
+
+
+'''print(f'o resultado é: {soma}')
+print(f'o resultado é: {div:.3f}') 
+print(f'o resultado é: {sub}')
+print(f'o resultado é: {multi}')
+print(f'o resultado é: {pot}')
+print(f'o resultado é: {divintera}')
+print(f'o resultado é: {modulo}')
+print()
+print(f'o numero digitado mais 1 é:{num1 + 1}')'''
+ 
+
+
+
---
+++ aula04.py
@@ -0,0 +1,12 @@
+nomes = ['shipin','patock','sigma']    
+print(nomes[0]) #esolhe o primeiro(0) da lista
+print(nomes[1:]) #do segundo(1) em diante
+print(nomes[:]) # retornar todos da lista
+print(nomes[1:2]) # numero no meio de 0 e 2 = 1
+print(nomes[:1]) # retornar os elementos ate o segundo(1)
+
+
+#as listas permitem colocar tipos de dados diferentes
+numeros = [1,2,3,4,5,6,7,8,9,'shipo','shipadas',1.5,True]
+print(numeros[0:11])
+
